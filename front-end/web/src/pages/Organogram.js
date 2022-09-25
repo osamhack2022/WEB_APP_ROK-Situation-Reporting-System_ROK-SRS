99b845ddd3d9b2b508ac449927716b35e51b609e
Yunseong Choe <chys3697@gmail.com>
2022-09-25 13:52:26 +0000

    temp: tree node increment

--- front-end/web/src/pages/Organogram.js
+++ front-end/web/src/pages/Organogram.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Tree, TreeNode } from 'react-organizational-chart';
 
 function Organogram() {
+    const [childNode, setChildNode] = useState([]);
+
+    const addChildNode = useCallback(() => {
+        setChildNode(node => [...node, <TreeNode label={node.length}/>])
+    }, [setChildNode]);
+
     return (
-        <Tree label="Parent">
-            <TreeNode label="Child" />
-        </Tree>
+        <>
+            <Tree label="Parent">
+                {childNode}
+            </Tree>
+            <button onClick={addChildNode}>+1</button>
+        </>
     )
 }
 
