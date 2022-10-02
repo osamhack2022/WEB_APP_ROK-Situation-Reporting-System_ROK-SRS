40d0ed061399e65b808ac55cc36de6c4c2368b12
Brian Kim <s3kim2018@berkeley.edu>
2022-10-02 14:59:09 +0000

    [Feat]: Register Template 초안 완성
    register.js, register,module.css 작성

--- front-end/web-next/pages/index.js
+++ front-end/web-next/pages/index.js
@@ -6,20 +6,22 @@ import NotLoggedinLayout from '../componenets/notloggedinlayout'
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>로그인</title>
-    </Head>
+      <Head>
+        <title>로그인</title>
+      </Head>
       <NotLoggedinLayout>
-        <h2 style = {{textAlign: 'center', marginBottom: '20px', marginTop: '20px'}}>로그인</h2>
-        <form action="/send-data-here" method="post" style = {{width: '250px', margin: 'auto'}}>
-          <input className={styles.inputfield} type="text" placeholder='군번' name="ID" style = {{margin: 'auto', display: 'block'}} required/>
+        <h1 style={{ textAlign: 'center', marginBottom: '20px', marginTop: '20px' }}>로그인</h1>
+        <form action="/send-data-here" method="post" style={{ width: '300px', margin: 'auto' }}>
+          <input className={styles.inputfield} type="text" placeholder='군번' name="ID" style={{ margin: 'auto', display: 'block' }} required />
           <br></br>
-          <input className={styles.inputfield} type="password" placeholder='비밀번호' name="password" required/>
+          <input className={styles.inputfield} type="password" placeholder='비밀번호' name="password" required />
           <br></br>
           <button className={styles.buttonfield} type="submit">로그인</button>
         </form>
         <div id={styles.loginfooter}>
-          <Link className={styles.links} href = "/register" style = {{color: "skyblue"}}>회원가입하기</Link>
+          <span className={styles.descfield}>계정이 없으세요? </span><Link href="/register" passHref><a className={styles.links}>회원가입하기</a></Link> <br></br>
+          <span className={styles.descfield}>비밀번호를 잃어버리셨나요? </span><Link href="/forgotpass" passHref><a className={styles.links}>비밀번호 찾기</a></Link>
+
         </div>
       </NotLoggedinLayout>
     </>
--- front-end/web-next/pages/register.js
+++ front-end/web-next/pages/register.js
@@ -1,4 +1,5 @@
 import NotLoggedinLayout from '../componenets/notloggedinlayout'
+import styles from '../styles/register.module.css'
 import Head from 'next/head'
 
 
@@ -10,8 +11,20 @@ const Register = () => {
             <title>회원가입</title>
         </Head>
         <NotLoggedinLayout>
-            <h2 style = {{textAlign: 'center', marginBottom: '20px', marginTop: '20px'}}>회원가입</h2>
-
+            <h1 style = {{textAlign: 'center', marginBottom: '20px', marginTop: '20px'}}>회원가입</h1>
+            <form action="/send-data-here" method="post" style={{ width: '300px', margin: 'auto' }}>
+                <input className={styles.inputfield} type="text" placeholder='이름' name="Name" style={{ margin: 'auto', display: 'block' }} required />
+                <br></br>
+                <input className={styles.inputfield} type="text" placeholder='군번' name="milnum" style={{ margin: 'auto', display: 'block' }} required />
+                <br></br>
+                <input className={styles.inputfield} type="text" placeholder='초대 코드' name="invcode" style={{ margin: 'auto', display: 'block' }} required />
+                <br></br>
+                <input className={styles.inputfield} type="password" placeholder='비밀번호' name="password" required />
+                <br></br>
+                <input className={styles.inputfield} type="confpassword" placeholder='비밀번호 확인' name="password" required />
+                <br></br>
+            <button className={styles.buttonfield} type="submit">가입하기</button>
+            </form>
         </NotLoggedinLayout>
         </>
     )
