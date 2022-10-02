b2f9a4cb9e3a1020b4d2be234d3982e33fee2942
Yunseong Choe <chys3697@gmail.com>
2022-10-02 14:20:40 +0000

    [Feat] apply user model
    백엔드의 유저 모델에 따라 field 추가

--- front-end/web/src/pages/Organogram.js
+++ front-end/web/src/pages/Organogram.js
@@ -10,7 +10,19 @@ function renderNode(node) {
     return (
         <TreeNode
             key={node.key}
-            label={<TreeNodeElement name={node.username} rank={node.militaryRank} roles={node.roles} />}
+            label={
+                <TreeNodeElement
+                    name={node.username}
+                    rank={node.militaryRank}
+                    department={node.department}
+                    position={node.position}
+                    roles={node.roles}
+                    snumber={node.serviceNumber}
+                    tel={node.phoneNumber}
+                    voip={node.voipNumber}
+                    email={node.email}
+                />
+            }
         >
             {node.children && node.children.map((childNode) => (renderNode(childNode)))}
         </TreeNode>
@@ -36,7 +48,13 @@ function TreeNodeElement(props) {
             >
                 <p><b>{props.name}</b></p>
                 <p>계급: {props.rank}</p>
+                <p>부서: {props.department}</p>
+                <p>직책: {props.position}</p>
                 <p>권한: {props.roles}</p>
+                <p>군번: {props.snumber}</p>
+                <p>전화번호: {props.tel}</p>
+                <p>군연락망: {props.voip}</p>
+                <p>이메일: {props.email}</p>
             </Card>
         </Button>
     )
@@ -46,31 +64,51 @@ function Organogram() {
     const myData = {
         'key': 1,
         'username': 'Kim',
+        'department': 'rok_srs',
+        'position': '보안팀장',
         'roles': 'editable',
         'militaryRank': '대위',
-        'pic': null,
+        'serviceNumber': '00-12345678',
+        'phoneNumber': '010-0000-1111',
+        'voipNumber': 'voip',
+        'email': '@@@',
         'children': [
             {
                 'key': 2,
                 'username': 'Jo',
+                'department': 'rok_srs',
+                'position': '백엔드',
                 'roles': 'viewable',
                 'militaryRank': '상병',
-                'pic': null,
+                'serviceNumber': '99-00112233',
+                'phoneNumber': '010-2222-3333',
+                'voipNumber': 'voip',
+                'email': '@@@',
                 'children': [{
                     'key': 4,
                     'username': 'Choe',
+                    'department': 'rok_srs',
+                    'position': '프론트엔드(WEB)',
                     'roles': 'none',
                     'militaryRank': '일병',
-                    'pic': null,
+                    'serviceNumber': '88-44556677',
+                    'phoneNumber': '010-4444-5555',
+                    'voipNumber': 'voip',
+                    'email': '@@@',
                     'children': null
                 }]
             },
             {
                 'key': 3,
                 'username': 'Kim',
+                'department': 'rok_srs',
+                'position': '프론트엔드(APP)',
                 'roles': 'viewable',
                 'militaryRank': '병장',
-                'pic': null,
+                'serviceNumber': '98-76543210',
+                'phoneNumber': '010-6666-7777',
+                'voipNumber': 'voip',
+                'email': '@@@',
                 'children': null
             },
         ]
