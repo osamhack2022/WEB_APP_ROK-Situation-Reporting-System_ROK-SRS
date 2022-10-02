7f5705882c1cb9ef3ef81ad6721b5572b3999f7f
Yunseong Choe <chys3697@gmail.com>
2022-10-02 11:08:32 +0000

    [Feat] organization tree template
    조직도 기본 요소 구상 및 디자인 기틀 형성

--- front-end/web/src/pages/Organogram.js
+++ front-end/web/src/pages/Organogram.js
@@ -1,11 +1,22 @@
 import React, { useState, useCallback } from 'react';
 import { Tree, TreeNode } from 'react-organizational-chart';
+import { Card } from 'antd';
+
+function TreeNodeElement(props) {
+    return (
+        <Card title={props.name} extra={<a href="#">추가</a>}>
+            <p>militaryRank: {props.rank}</p>
+            <p>roles: {props.roles}</p>
+        </Card>
+    )
+}
 
 function Organogram() {
     const [childNode, setChildNode] = useState([]);
+    const myData = { "key": 1, "username": "Choe", "roles": "editable", "militaryRank": "private", "pic": null };
 
     const addChildNode = useCallback(() => {
-        setChildNode(node => [...node, <TreeNode label={node.length}/>])
+        setChildNode(node => [...node, <TreeNode label={<TreeNodeElement name={myData.username} rank={myData.militaryRank} roles={myData.roles} />}/>])
     }, [setChildNode]);
 
     return (
