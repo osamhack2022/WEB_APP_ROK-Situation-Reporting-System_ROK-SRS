f6cdd49b1538c2a7404797d9a5e77191f7079d36
Brian Kim <s3kim2018@berkeley.edu>
2022-10-02 07:19:35 +0000

    [Feat]: 로그인 패이지 추가, 기본 form 완성
    프론트엔드 로그인 폼 완성 및 배경화면/로고 추가.
    로그인 페이지는 index.js로 프론트엔드 서버 시작시 표시됨.

--- front-end/web-next/pages/index.js
+++ front-end/web-next/pages/index.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import background from '../img/marpat2.jpg'
+import logo from '../img/logotransparent.png'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hi</h1>
+    <>
+    <div className={styles.container} style = {{
+      backgroundImage: `url(${background.src})`,
+      width: '100%',
+      height: '100%',
+    }}>
+      <div id={styles.idcontainer}>
+        <div id = {styles.logocontainer}>
+          <Image id={styles.logo} src = {logo.src} width = '450px' height = '150px'/>
+
+        </div>
+        <form action="/send-data-here" method="post" style = {{display: 'block', width: '450px'}}>
+          <input className={styles.inputfield} type="text" name="ID" style = {{margin: 'auto'}} />
+          <br></br>
+          <input className={styles.inputfield} type="password" name="password" />
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+
+      </div>
     </div>
+    </>
   )
 }
