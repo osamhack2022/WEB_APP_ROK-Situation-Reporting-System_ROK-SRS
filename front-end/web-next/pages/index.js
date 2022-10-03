0f2c136ea32ec4a6fa742d5fc9904ffe24210c28
Brian Kim <s3kim2018@berkeley.edu>
2022-10-03 06:43:41 +0000

    [Feat]: 로그인 페이지 데모 섹션 추가
    병사 및 대대장 데모계정 로그인 버튼 추가.
    데모부대: 수도방위사령부 제1방공여단 3대대
    대대장: 중령 김기철
    병사: 상병 김형민

--- front-end/web-next/pages/index.js
+++ front-end/web-next/pages/index.js
@@ -18,6 +18,16 @@ export default function Home() {
           <br></br>
           <button className={styles.buttonfield} type="submit">로그인</button>
         </form>
+        <div id = {styles.demofooter}>
+          <h2 id = {styles.demofooterh2}>데모</h2>
+          <span className={styles.descfield}>대대장님 계정 데모: </span> <button>대대장 계정으로 로그인</button> <br></br>
+          <span className={styles.desctext}>수도방위사령부 제1방공여단 3대대 대대장 중령 김기철의 계정을 채험해 보세요!</span> <br></br>
+          <span className={styles.descfield}>용사 계정 데모: </span> <button>용사 계정으로 로그인</button> <br></br>
+          <span className={styles.desctext}>수도방위사령부 제1방공여단 3대대에서 근무중인 용사 상병 김형민의 계정을 채험해 보세요!</span> <br></br>
+
+        </div>
+
+
         <div id={styles.loginfooter}>
           <span className={styles.descfield}>계정이 없으세요? </span><Link href="/register" passHref><a className={styles.links}>회원가입하기</a></Link> <br></br>
           <span className={styles.descfield}>비밀번호를 잃어버리셨나요? </span><Link href="/forgotpass" passHref><a className={styles.links}>비밀번호 찾기</a></Link>
