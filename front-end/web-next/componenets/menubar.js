b4ce2c53c06ef191cc76551f0ab1b8dc70a62c49
Brian Kim <s3kim2018@berkeley.edu>
2022-10-04 14:55:54 +0000

    [Feat]: Menubar Menu Item 추가
    아이콘, 링크, hovereffect not yet finished

--- front-end/web-next/componenets/menubar.js
+++ front-end/web-next/componenets/menubar.js
@@ -1,17 +1,63 @@
 import styles from '../styles/menubar.module.css'
-import homebutton from '../img/homebutton.png'
-import Image from 'next/image'
+import Link from "next/link"
+import { HomeFilled, TeamOutlined } from '@ant-design/icons';
 
+const links = [
+    {
+        href: "/userhome",
+        icon: HomeFilled,
+        label: "Home"
+    },
+    {
+        href: "/userhome",
+        icon: TeamOutlined,
+        label: "조직도"
+    },
+    {
+        href: "/userhome",
+        icon: HomeFilled,
+        label: "메모보고"
+    },
+    {
+        href: "/userhome",
+        icon: HomeFilled,
+        label: "메세지"
+    },
+    {
+        href: "/userhome",
+        icon: HomeFilled,
+        label: "알림"
+    },
+    {
+        href: "/userhome",
+        icon: HomeFilled,
+        label: "세팅"
+    }
+]
 
-const MenuBar = ({children}) => {
+const MenuBar = ({ children }) => {
     return (
         <>
-            <div id = {styles.menubar}>
-            <Image id={styles.homebutton} src = {homebutton.src} width = '28px' height = '24px' style = {{margin: 'auto'}}/>
+            <div className={styles.menubar}>
+                {links.map(({
+                    href,
+                    icon: Icon,
+                    label
+                }, i) =>
+                    <Link href={href} >
+                        <a className={styles.menuitem} style={{
+                            marginTop: i === 0 || i === 1 ? '55px' : '32px'
 
-            
+                        }}>
+                            <Icon className={styles.icons} />
+                            <span>{label}</span>
+                        </a>
+                    </Link>
+                )}
+
+
             </div>
-            <div id = {styles.child}>
+            <div id={styles.child}>
                 {children}
             </div>
         </>
