79ad819a5fbc0eef5253589a33c480902c63a5b3
Yunseong Choe <chys3697@gmail.com>
2022-10-04 14:14:28 +0000

    [Feat]Nested comment cards
    메모 보고에 댓글의 답글 형식 제작

--- front-end/web-next/componenets/MemoReport.js
+++ front-end/web-next/componenets/MemoReport.js
@@ -1,31 +1,33 @@
 import { Avatar, Button, Comment } from 'antd';
 
-function ReportCard(props) {
-    function ButtonGroup() {
-        return (
-            <>
-                <Button
-                    type="dashed"
-                    danger
-                >
-                    종결하기
-                </Button>
-                <Button>
-                    상급보고
-                </Button>
-            </>
-        )
-    }
-
+function ReportCard(props, { children }) {
+  function ButtonGroup() {
     return (
-        <Comment
-            author={props.name}
-            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-            content={props.memo}
-            datetime={props.datetime}
-            actions={<ButtonGroup />}
-        />
+      <>
+        <Button
+          type="dashed"
+          danger
+        >
+          종결하기
+        </Button>
+        <Button>
+          상급보고
+        </Button>
+      </>
     )
+  }
+
+  return (
+    <Comment
+      author={props.name}
+      avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+      content={props.memo}
+      datetime={props.datetime}
+      actions={<ButtonGroup />}
+    >
+      {children}
+    </Comment>
+  )
 }
 
 export default ReportCard;
--- front-end/web-next/pages/memo.js
+++ front-end/web-next/pages/memo.js
@@ -1,17 +1,55 @@
 import Head from 'next/head'
 import ReportCard from '../componenets/MemoReport'
 
+function listCard(data) {
+  if(!data)
+    return null;
+
+  return (
+    data.map((node) => (
+      <ReportCard
+        name={node.name}
+        memo={node.memo}
+        datetime={node.datetime}
+      >
+        { node.children && listCard(node.children) }
+      </ReportCard>
+    ))
+  )
+}
+
 export default function Memo() {
-    return (
-        <>
-            <Head>
-                <title>메모 보고</title>
-            </Head>
-            <ReportCard
-                name="Choe"
-                memo="TEST"
-                datetime="1분 전"
-            />
-        </>
-    )
+  const sampleData = [
+    {
+      'name': 'Choe',
+      'memo': '거수자 1명 식별',
+      'datetime': '1분 전'
+    },
+    {
+      'name': 'Kim',
+      'memo': '위병소 앞 화재 발생',
+      'datetime': '23분 전',
+      'children': [
+        {
+          'name': 'Kim',
+          'memo': '화재 상황 종료',
+          'datetime': '16분 전'
+        }
+    ]
+    },
+    {
+      'name': 'Jo',
+      'memo': '미상의 비행체 관측',
+      'datetime': '1시간 전'
+    }
+  ]
+
+  return (
+    <>
+      <Head>
+        <title>메모 보고</title>
+      </Head>
+      {listCard(sampleData)}
+    </>
+  )
 }
