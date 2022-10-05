96a0b26faf420a9a46b301e7a9258329cb116810
Brian Kim <s3kim2018@berkeley.edu>
2022-10-05 11:46:49 +0000

    [Feat]: Menubar 개발 완료
    Hover effect, icon 개발 완료
    Route설정 필요

--- front-end/web-next/componenets/menubar.js
+++ front-end/web-next/componenets/menubar.js
@@ -1,6 +1,10 @@
 import styles from '../styles/menubar.module.css'
 import Link from "next/link"
 import { HomeFilled, TeamOutlined } from '@ant-design/icons';
+import { RiOrganizationChart, RiLogoutBoxLine } from 'react-icons/ri';
+import { GrNotes } from 'react-icons/gr';
+import { AiOutlineMessage, AiOutlineBell, AiOutlineSetting } from 'react-icons/ai';
+
 
 const links = [
     {
@@ -10,29 +14,29 @@ const links = [
     },
     {
         href: "/userhome",
-        icon: TeamOutlined,
+        icon: RiOrganizationChart,
         label: "조직도"
     },
     {
         href: "/userhome",
-        icon: HomeFilled,
+        icon: GrNotes,
         label: "메모보고"
     },
     {
         href: "/userhome",
-        icon: HomeFilled,
+        icon: AiOutlineMessage,
         label: "메세지"
     },
     {
         href: "/userhome",
-        icon: HomeFilled,
+        icon: AiOutlineBell,
         label: "알림"
     },
     {
         href: "/userhome",
-        icon: HomeFilled,
+        icon: AiOutlineSetting,
         label: "세팅"
-    }
+    }, 
 ]
 
 const MenuBar = ({ children }) => {
@@ -46,7 +50,7 @@ const MenuBar = ({ children }) => {
                 }, i) =>
                     <Link href={href} >
                         <a className={styles.menuitem} style={{
-                            marginTop: i === 0 || i === 1 ? '55px' : '32px'
+                            marginTop: i === 0 || i === 1 ? '40px' : '12px'
 
                         }}>
                             <Icon className={styles.icons} />
@@ -54,6 +58,12 @@ const MenuBar = ({ children }) => {
                         </a>
                     </Link>
                 )}
+                <Link href = "/userhome">
+                    <a className={styles.logoutitem}>
+                        <RiLogoutBoxLine></RiLogoutBoxLine>
+                        <span>로그아웃</span>
+                    </a>
+                </Link>
 
 
             </div>
