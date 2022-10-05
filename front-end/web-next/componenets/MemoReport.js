c8c2c29c2bf7cf6fdfbdbd2ba629035d1d91961d
Yunseong Choe <chys3697@gmail.com>
2022-10-05 14:48:27 +0000

    [Feat]Memo report template
    메모 보고 레이아웃 초안 작성
    - content 부분 완성
    - 추후 sider 제작 후 디자인 예정

--- front-end/web-next/componenets/MemoReport.js
+++ front-end/web-next/componenets/MemoReport.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Comment, Layout, Divider } from 'antd';
+import { Avatar, Button, Comment, List } from 'antd';
 
 function ReportCard(props) {
   function ButtonGroup() {
@@ -27,67 +27,51 @@ function ReportCard(props) {
       >
         {props.children}
       </Comment>
-      <ButtonGroup />
+      {!props.isChild && <ButtonGroup />}
     </>
   )
 }
 
-function ReportLayout() {
-	return (
-		<Layout>
-			<Layout.Sider style={styles.siderLayout}>
-				<div style={styles.siderMenu}>
-					<p>받은 메모 보고</p>
-				</div>
-			</Layout.Sider>
-			<Layout.Content style={styles.contentLayout}>
-				<div style={styles.contentMenu}>
-					<p>3초소 거수자 발견[미종결]</p>
-					<p>종류: 긴급사항</p>
-					<p>중요도: 5</p>
-					<p>2022-08-03 03:34</p>
-					<Divider />
-					<ReportCard
-						name="상병 조용효"
-						memo="충성! 당직사령닙 3초소에 사복을 입은 거수자가 나타났습니다.\n무기를 소지한것 같지는 않고 위병소 앞에서 두리번 거리고 있습니다.\n현재 경계중이며, 추가사항 발생시 보고드리겠습니다. "
-						datetime="2022-08-03 03:34"
-					/>
-					<Divider />
-					<ReportCard
-						name="상병 조용효"
-						memo="충성! 당직사령닙 3초소에 사복을 입은 거수자가 나타났습니다.\n무기를 소지한것 같지는 않고 위병소 앞에서 두리번 거리고 있습니다.\n현재 경계중이며, 추가사항 발생시 보고드리겠습니다. "
-						datetime="2022-08-03 03:34"
-					/>
-					<Divider />
-					<p>보고 체계: 중대</p>
-				</div>
-			</Layout.Content>
-		</Layout>
-	)
+function replyList(data) {
+  if (!data)
+    return null;
+
+  return (
+    data.map((node) => (
+      <ReportCard
+        key={node.key}
+        name={node.name}
+        memo={node.memo}
+        datetime={node.datetime}
+        isChild
+      >
+        {node.children && listCard(node.children)}
+      </ReportCard>
+    ))
+  )
+}
+
+function ReportList(props) {
+  return (
+    <List
+      itemLayout="vertical"
+      header={props.header && props.header}
+      footer={props.footer && props.footer}
+      dataSource={props.data}
+      renderItem={(item) => (
+        <List.Item>
+          <ReportCard
+            name={item.name}
+            memo={item.memo}
+            datetime={item.datetime}
+          >
+            {item.children && replyList(item.children)}
+          </ReportCard>
+        </List.Item>
+      )}
+    />
+  )
 }
 
 export default ReportCard;
-export { ReportLayout };
-
-const styles = {
-  siderLayout: {
-    backgroundColor: 'transparent',
-    padding: '10px 20px'
-  },
-  siderMenu: {
-    padding: '10px 20px',
-    backgroundColor: '#fff',
-    borderRadius: '20px',
-    boxShadow: '3px 4px 5px #777'
-  },
-  contentLayout: {
-    backgroundColor: 'transparent',
-    padding: '10px 20px'
-  },
-  contentMenu: {
-    padding: '10px 20px',
-    backgroundColor: '#fff',
-    borderRadius: '20px',
-    boxShadow: '3px 4px 5px #777'
-  }
-}
+export { ReportList }
--- front-end/web-next/pages/memo.js
+++ front-end/web-next/pages/memo.js
@@ -1,23 +1,6 @@
 import Head from 'next/head'
-import ReportCard, {ReportLayout} from '../componenets/MemoReport'
-
-function listCard(data) {
-  if(!data)
-    return null;
-
-  return (
-    data.map((node) => (
-      <ReportCard
-        key={node.key}
-        name={node.name}
-        memo={node.memo}
-        datetime={node.datetime}
-      >
-        { node.children && listCard(node.children) }
-      </ReportCard>
-    ))
-  )
-}
+import { Layout, Divider, List } from 'antd';
+import ReportCard, { ReportList } from '../componenets/MemoReport'
 
 export default function Memo() {
   const sampleData = [
@@ -39,7 +22,7 @@ export default function Memo() {
           'memo': '화재 상황 종료',
           'datetime': '16분 전'
         }
-    ]
+      ]
     },
     {
       'key': '4',
@@ -55,7 +38,53 @@ export default function Memo() {
         <title>메모 보고</title>
       </Head>
       {/* {listCard(sampleData)} */}
-      <ReportLayout />
+      <Layout>
+        <Layout.Sider style={styles.siderLayout}>
+          <div style={styles.siderMenu}>
+            <p>받은 메모 보고</p>
+          </div>
+        </Layout.Sider>
+        <Layout.Content style={styles.contentLayout}>
+          <div style={styles.contentMenu}>
+            <ReportList
+              header={
+                <>
+                  <p>3초소 거수자 발견[미종결]</p>
+                  <p>종류: 긴급사항</p>
+                  <p>중요도: 5</p>
+                  <p>2022-08-03 03:34</p>
+                </>
+              }
+              footer={<p>보고 체계: 중대</p>}
+              data={sampleData}
+            />
+          </div>
+        </Layout.Content>
+      </Layout>
     </>
   )
+}
+
+
+const styles = {
+  siderLayout: {
+    backgroundColor: 'transparent',
+    padding: '10px 20px'
+  },
+  siderMenu: {
+    padding: '10px 20px',
+    backgroundColor: '#fff',
+    borderRadius: '20px',
+    boxShadow: '3px 4px 5px #777'
+  },
+  contentLayout: {
+    backgroundColor: 'transparent',
+    padding: '10px 20px'
+  },
+  contentMenu: {
+    padding: '10px 20px',
+    backgroundColor: '#fff',
+    borderRadius: '20px',
+    boxShadow: '3px 4px 5px #777'
+  }
 }
