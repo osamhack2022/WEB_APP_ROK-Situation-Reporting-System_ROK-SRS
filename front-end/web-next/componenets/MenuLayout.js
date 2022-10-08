3412a9c6f3d7eef718066b4ef234455c4e0afba4
Yunseong Choe <chys3697@gmail.com>
2022-10-08 14:55:39 +0000

    [Design]Styling menu layout #42

--- front-end/web-next/componenets/MenuLayout.js
+++ front-end/web-next/componenets/MenuLayout.js
@@ -1,7 +1,6 @@
-import { Layout, Menu } from 'antd';
-import { HomeFilled } from '@ant-design/icons';
+import { Layout, Menu, Button } from 'antd';
+import { HomeFilled, FileTextOutlined } from '@ant-design/icons';
 import { RiOrganizationChart, RiLogoutBoxLine } from 'react-icons/ri';
-import { GrNotes } from 'react-icons/gr';
 import { AiOutlineMessage, AiOutlineBell, AiOutlineSetting } from 'react-icons/ai';
 import { useRouter } from "next/router"
 import styles from '../styles/MenuLayout.module.css';
@@ -13,33 +12,33 @@ function MenuLayout(props) {
   const sideMenu = [
     {
       key: 'home',
-      icon: <HomeFilled />,
-      label: 'Home'
+      icon: <HomeFilled className={styles.menuIcon} style={{ fontSize: '18pt' }} />,
+      label: <span className={styles.menuLabel}>Home</span>
     },
     {
       key: 'orgchart',
-      icon: <RiOrganizationChart />,
-      label: '조직도'
+      icon: <RiOrganizationChart className={styles.menuIcon} />,
+      label: <span className={styles.menuLabel}>조직도</span>
     },
     {
       key: 'memonote',
-      icon: <GrNotes />,
-      label: '메모 보고'
+      icon: <FileTextOutlined className={styles.menuIcon} style={{ fontSize: '18pt' }} />,
+      label: <span className={styles.menuLabel}>메모 보고</span>
     },
     {
       key: 'messeges',
-      icon: <AiOutlineMessage />,
-      label: '메세지'
+      icon: <AiOutlineMessage className={styles.menuIcon} />,
+      label: <span className={styles.menuLabel}>메세지</span>
     },
     {
       key: 'notifications',
-      icon: <AiOutlineBell />,
-      label: '알림'
+      icon: <AiOutlineBell className={styles.menuIcon} />,
+      label: <span className={styles.menuLabel}>알림</span>
     },
     {
       key: 'settings',
-      icon: <AiOutlineSetting />,
-      label: '설정'
+      icon: <AiOutlineSetting className={styles.menuIcon} />,
+      label: <span className={styles.menuLabel}>설정</span>
     }
   ];
 
@@ -54,7 +53,7 @@ function MenuLayout(props) {
           className={styles.sidebar}
           mode="vertical"
           items={sideMenu}
-          defaultSelectedKeys={currentPath.substring(1, (currentPath.indexOf('/', 1) === -1 ? currentPath.length : currentPath.indexOf('/')))}
+          selectedKeys={currentPath.substring(1, (currentPath.indexOf('/', 1) === -1 ? currentPath.length : currentPath.indexOf('/')))}
           onSelect={({ key }) => router.push('/' + key)}
         />
       </Layout.Sider>
