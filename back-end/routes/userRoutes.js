6d730bddc21ca1355761aa557b1a8b002c0f85e6
yhcho0405 <yhcho0405@kakao.com>
2022-10-11 18:25:23 +0900

    Backup: Restore accidentally deleted files(commit SHA : 3edff8d095fc13e1f5febd796817152c75015e5e)

---
+++ back-end/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+  addUser
+} = require("../controllers/userControllers");
+const { protect, onlyAdmin } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.route("/").get(protect, allUsers);
+router.route("/add").post(onlyAdmin, addUser);
+router.route("/register").post(registerUser);
+router.post("/login", authUser);
+
+module.exports = router;
