bc6b7ba6e1fd11d02a0c082da9e353d06a43f955
Brian Kim <s3kim2018@berkeley.edu>
2022-10-12 09:43:15 +0000

    [Feat]: 로그인 페이지 벡엔드 연동 완료

--- front-end/web-next/pages/index.js
+++ front-end/web-next/pages/index.js
@@ -3,9 +3,46 @@ import styles from '../styles/Home.module.css'
 import Link from "next/link"
 import NotLoggedinLayout from '../componenets/notloggedinlayout'
 import { Avatar, Divider, List, Skeleton, Button, Input, Radio, message, Upload, Image, TreeSelect, Form } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { ConsoleSqlOutlined } from '@ant-design/icons';
+import { setCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
 
+const backendroot = process.env.NEXT_PUBLIC_BACKEND_ROOT
 
 export default function Home() {
+  const router = useRouter()
+  const [DoDID, setDoDID] = useState();
+  const [Password, setPassword] = useState(); 
+  const [error, setError] = useState('');
+  let endpoint = backendroot + 'api/user/login'
+  let submitlogin = async (event) => {
+    const data = {
+      DoDID: DoDID,
+      password: Password
+    }
+    const JSONdata = JSON.stringify(data)
+    const options = {
+        // The method is POST because we are sending data.
+        method: 'POST',
+        // Tell the server we're sending JSON.
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body of the request is the JSON data we created above.
+        body: JSONdata,
+    }
+    const response = await fetch(endpoint, options)
+    const result = await response.json()
+    console.log(result)
+    if (result.token) {
+      setCookie('usercookie', result.token)
+      router.push('/home')
+      console.log("submit login")
+    } else {
+      setError(result.message)
+    }
+  }
   return (
     <>
       <Head>
@@ -13,18 +50,18 @@ export default function Home() {
       </Head>
       <NotLoggedinLayout>
         <h1 style={{ textAlign: 'center', marginBottom: '20px', marginTop: '20px' }}>로그인</h1>
-        <Form action="/send-data-here" method="post" style={{ width: '400px', margin: 'auto' }}>
+        <Form method="post" style={{ width: '400px', margin: 'auto' }} onFinish = {submitlogin}>
           <Form.Item name="군번" rules={[{ required: true, message: '군번을 입력해 주세요' }]}>
-            <Input placeholder = "군번" />
+            <Input placeholder = "군번" onChange={(event) => { setDoDID(event.target.value) }} />
           </Form.Item>
-          <Form.Item name="군번" rules={[{ required: true, message: '비밀번호를 입력해 주세요' }]}>
-            <Input.Password placeholder = "비밀번호" />
+          <Form.Item name="password" rules={[{ required: true, message: '비밀번호를 입력해 주세요' }]}>
+            <Input.Password placeholder = "비밀번호" onChange={(event) => { setPassword(event.target.value) }} />
           </Form.Item>
           <br></br>
           <Form.Item>
               <div style = {{display: 'flex'}}>
                   <button className={styles.buttonfield} type="primary" htmlType="submit">로그인</button>
-                  <p id = {styles.error}>Error Message</p>
+                  <p id = {styles.error}>{error}</p>
               </div>
           </Form.Item>
         </Form>
