d71e283bd1742e115c1eb795583eb8a63567dd0c
hyeongmin Kim <113012760+gudmin0526@users.noreply.github.com>
2022-10-12 14:47:12 +0000

    Design: #75

--- front-end/app/src/screens/setting-screens/UserMgtScreen.js
+++ front-end/app/src/screens/setting-screens/UserMgtScreen.js
@@ -1,13 +1,56 @@
 import React, { useState } from 'react'
-import { FAB } from 'react-native-paper'
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { FAB, List } from 'react-native-paper'
+import { SafeAreaView, StyleSheet, ScrollView } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+
+const LeftImage = () => (
+  <Avatar.Image
+    source={require('../../assets/images/avatar.png')}
+    size={40}
+    style={{ alignSelf: 'center', marginLeft: 15, marginRight: 5 }}
+  />
+)
+
+const tempData = [
+  {
+    Name: '김형민',
+    position: '본부중대 통신',
+  },
+]
+
+const deleteUserHandler = () => {}
 
 export function UserMgtScreen() {
   const navigation = useNavigation()
 
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {tempData.map((user, idx) => (
+          <List.Item
+            title={user.Name}
+            description={user.position}
+            left={LeftImage}
+            right={() => (
+              <List.Icon
+                icon="minus-circle-outline"
+                onPress={() => deleteUserHandler()}
+              />
+            )}
+            titleStyle={styles.titleStyle}
+            descriptionStyle={styles.descriptionStyle}
+            key={idx}
+            onPress={() =>
+              props.showModal({
+                name: user.title,
+                role: user.description,
+                team: '통신소대',
+                tel: '010-1234-5678',
+              })
+            }
+          />
+        ))}
+      </ScrollView>
       <FAB
         icon="account-plus"
         style={styles.fab}
