38b41f169b4554147610a40a13f2efb526e35251
Brian Kim <s3kim2018@berkeley.edu>
2022-10-13 14:55:41 +0000

    [Feat]: 보고체계 설정 패이지 추가 #77

---
+++ front-end/web-next/pages/settings/reportsystem.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from "next/link"
+import RegisterHeader from '../../componenets/registerheader'
+
+
+const ReportSystem = () => {
+    return <>
+        <RegisterHeader>
+            보고체계 재작, 수정, 삭제
+        </RegisterHeader>
+    </>
+}
+
+export default ReportSystem;
