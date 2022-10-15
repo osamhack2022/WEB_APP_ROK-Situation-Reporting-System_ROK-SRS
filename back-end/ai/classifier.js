279688f3e0b5b9ac24cf0d7795683063d7e0f9f0
Brian Kim <s3kim2018@berkeley.edu>
2022-10-15 15:39:56 +0000

    [Feat] #91 built pos_tokenizer

--- back-end/ai/classifier.js
+++ back-end/ai/classifier.js
@@ -1,3 +1,21 @@
 import TheClassifier from 'classificator'
 import translate from 'translate-google'
 import pos from 'pos';
+
+
+async function pos_tokenizer(text) {
+    const engtext = await translate(text, {to: 'en'});
+    var words = new pos.Lexer().lex(engtext);
+    var tagger = new pos.Tagger();
+    var taggedWords = tagger.tag(words);
+    let builtword = ''
+    for (let i in taggedWords) {
+        var taggedWord = taggedWords[i];
+        var word = taggedWord[0];
+        var tag = taggedWord[1];
+        if (tag.includes('N') || tag.includes('V') || tag.includes('A'))
+        builtword = builtword + word + " "
+    }
+    console.log(builtword)
+    return builtword
+} 
