8a721a6ef926403feb1500a999f5510add55776b
Brian Kim <s3kim2018@berkeley.edu>
2022-10-15 15:40:34 +0000

    [Feat]: #91  Added trainiable datasets

--- back-end/ai/classifier.js
+++ back-end/ai/classifier.js
@@ -1,7 +1,50 @@
 import TheClassifier from 'classificator'
 import translate from 'translate-google'
 import pos from 'pos';
-
+let positive = [
+    'Questions',
+    'Suggestions',
+    'will there be',
+    'dining together',
+    'food',
+    'Go to work',
+    'clean',
+    'break the ice',
+    `I love tacos.`,
+    `Dude, that burrito was epic!`,
+    `Holy cow, these nachos are so good and tasty.`,
+    `I am drooling over the awesome bean and cheese quesadillas.`
+]
+var negative = [
+  `Suicide`,
+  `urgent matter`,
+  'emergency situation',
+  `dead`,
+  'wire',
+  `secret`,
+  `classified`,
+  `attacked`,
+  `firearm`,
+  `unknown man has appeared`,
+  `in front of the guardhouse`,
+  `there are suspicious personnel`,
+  `in possession of a firearm`,
+  `A suspicious person has been found`,
+  `a landmine is in front of you`,
+  `My unit has been severly hurt`,
+  `hail weather snow advisory`,
+  `dangerous situation`,
+  '5 minute waiting group',
+  'North Korean provocation',
+  'Missle Launch',
+  'ammunition distribution',
+  'unusual occurrence',
+  `Gross, worst taco ever.`,
+  `The buritos gave me horrible diarrhea.`,
+  `I'm going to puke if I eat another bad nacho.`,
+  `I'd rather die than eat those nasty enchiladas.`,
+  'special has happened in the dormitory'
+]
 
 async function pos_tokenizer(text) {
     const engtext = await translate(text, {to: 'en'});
