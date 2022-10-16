ca6c96534f6e9e201e231789fef5c847bb9b553f
Brian Kim <s3kim2018@berkeley.edu>
2022-10-16 06:26:26 +0000

    [Refactor]: #92 used safer libraries

--- back-end/ai.example.js
+++ back-end/ai.example.js
@@ -1,10 +1,9 @@
 const getScore = require('./ai/classifier.js')
 
 async function getscorewrapper(){ 
-    let score  = await getScore('위병소 앞 총기를 든 거수자들이 나타났습니다. 자살하고싶어')
+    let score  = await getScore('위병소 앞 총기를 든 거수자들이 나타났습니다. 자살하고싶어') //should get 5
     console.log(score)
-    let score2 = await getScore('당직사관님, 다음 회식때 피자를 먹고싶습니다!')
+    let score2 = await getScore('당직사관님, 다음 회식때 피자를 먹고싶습니다!') //should get 2
     console.log(score2)
-
 }
 getscorewrapper()
