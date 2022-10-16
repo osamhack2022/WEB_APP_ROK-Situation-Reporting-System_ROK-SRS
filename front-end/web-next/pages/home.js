c5f7f662297f44e96cab9b04f835693fb5721e12
Brian Kim <s3kim2018@berkeley.edu>
2022-10-16 09:07:50 +0000

    [Feat]: #96 부대 로고, 부대 정보

--- front-end/web-next/pages/home.js
+++ front-end/web-next/pages/home.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { Middleware } from 'next/dist/lib/load-custom-routes'
 import MenuBar from '../componenets/menubar'
 import style from '../styles/homepage.module.css'
-import {Image} from 'antd'
+import Image from 'next/image'
 import unitlogo from '../img/unitlogo.png'
 
 const Home = () => {
@@ -13,30 +13,28 @@ const Home = () => {
         </Head>
         <div className = {style.leftbar}>
             <div className = {style.user}>
-                <div style = {{margin: 'auto', width: '160px', height: '160px'}}><Image width = {160} height = {160} src="https://camo.githubusercontent.com/3c2bd3f35721dc332ebf2b11ace89722c37a0f60b94eac42b2a0462fdeb2d420/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f363134322f363134323232362e706e67"/></div>
-                <div style = {{marginTop: '10px', width: '160px', margin: 'auto'}}>
-                    <span className={style.rank}>대위</span> <span className={style.name}>OOO</span> <br/>
-                    <span className={style.role}>정보통신운용장교</span>
-                </div>
+                <div style = {{margin: 'auto', width: '180px', height: '180px'}}><Image style={{marginTop: '20px'}} width = {180} height = {180} src="https://camo.githubusercontent.com/3c2bd3f35721dc332ebf2b11ace89722c37a0f60b94eac42b2a0462fdeb2d420/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f363134322f363134323232362e706e67"/></div>
                 
             </div>
             <div className = {style.unit}>
-                <p>소속부대</p>
-                <div style = {{marginTop: '30px', marginLeft: '15px'}}><Image width = {160} height = {200} src={unitlogo.src}/></div>
+                <div className = {style.orgunit}>소속부대</div>
+                <div style = {{marginTop: '10px', marginLeft: '20px'}}><Image width = {180} height = {220} src={unitlogo.src}/></div>
                 <div>
-                <p>수도방위사령부</p>
+                <p className = {style.unitmotto}>충성, 명예, 단결 <br></br> 살아방패 죽어충성</p>
 
                 </div>
                 
             </div>
         </div>
-        <div className = {style.secondrow}>
-
+        <div className = {style.mainarea}>
+        <div style = {{marginTop: '10px', width: '160px', margin: 'auto'}}>
+                    <span className={style.rank}>대위</span> <span className={style.name}>OOO</span> <br/>
+                    <span className={style.role}>정보통신운용장교</span>
+                </div>
         </div>
 
 
         
-        <p>홈페이지</p>
     </>
 }
 
