4236469218b6a1e19e5c2d37ea905f76f361d56a
Brian Kim <s3kim2018@berkeley.edu>
2022-10-16 08:04:27 +0000

    [Feat]: Added profil pic on home page

--- front-end/web-next/pages/home.js
+++ front-end/web-next/pages/home.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { Middleware } from 'next/dist/lib/load-custom-routes'
 import MenuBar from '../componenets/menubar'
 import style from '../styles/homepage.module.css'
-import Image from 'next/image'
+import {Image} from 'antd'
 import unitlogo from '../img/unitlogo.png'
 
 const Home = () => {
@@ -11,14 +11,31 @@ const Home = () => {
         <Head>
             <title>홈페이지</title>
         </Head>
-        <div className = {style.unit}>
-            <div style = {{marginTop: '30px', marginLeft: '15px'}}><Image width = {200} height = {240} src={unitlogo.src}/></div>
-            <div>
-            <p>수도방위사령부</p>
-
+        <div className = {style.leftbar}>
+            <div className = {style.user}>
+                <div style = {{margin: 'auto', width: '160px', height: '160px'}}><Image width = {160} height = {160} src="https://camo.githubusercontent.com/3c2bd3f35721dc332ebf2b11ace89722c37a0f60b94eac42b2a0462fdeb2d420/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f363134322f363134323232362e706e67"/></div>
+                <div style = {{marginTop: '10px', width: '160px', margin: 'auto'}}>
+                    <span className={style.rank}>대위</span> <span className={style.name}>OOO</span> <br/>
+                    <span className={style.role}>정보통신운용장교</span>
+                </div>
+                
             </div>
-            
+            <div className = {style.unit}>
+                <p>소속부대</p>
+                <div style = {{marginTop: '30px', marginLeft: '15px'}}><Image width = {160} height = {200} src={unitlogo.src}/></div>
+                <div>
+                <p>수도방위사령부</p>
+
+                </div>
+                
+            </div>
         </div>
+        <div className = {style.secondrow}>
+
+        </div>
+
+
+        
         <p>홈페이지</p>
     </>
 }
