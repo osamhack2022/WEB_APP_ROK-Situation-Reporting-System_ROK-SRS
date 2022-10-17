792eafd9a077839c731c38af071c370210b81133
Brian Kim <s3kim2018@berkeley.edu>
2022-10-17 10:13:28 +0000

    [Feat]: #99  Added Page Headers for Home and Settings

--- front-end/web-next/pages/settings.js
+++ front-end/web-next/pages/settings.js
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Link from "next/link"
 import MenuBar from '../componenets/menubar'
 import RegisterHeader from '../componenets/registerheader';
-import { Dropdown, Menu, Space } from 'antd';
+import { Dropdown, Menu, Space, PageHeader, Breadcrumb } from 'antd';
+import styles from '../styles/settingsmenubar.module.css'
 import { DownOutlined } from '@ant-design/icons';
 import { useRouter } from "next/router"
-
 
 
 
@@ -13,7 +13,20 @@ import { useRouter } from "next/router"
 const Settings = () => {
   
     return <>
-        <RegisterHeader></RegisterHeader>
+            <PageHeader className="site-page-header" title="계정설정"
+            breadcrumb={  
+            <Breadcrumb>
+                <Breadcrumb.Item style = {{color: 'black', cursor: 'pointer'}}>계정설정</Breadcrumb.Item>
+                <Breadcrumb.Item>
+                  <a href="settings/unit">부대설정</a>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item className = {styles.lastitem}>
+                  <a href="settings/reportsystem">보고체계 설정</a>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <a style = {{display: 'none'}}>hi</a>
+                </Breadcrumb.Item>
+              </Breadcrumb> } style={{backgroundColor: "white",  boxShadow: 'inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255), 0.3em 0.3em 1em rgba(0, 0, 0, 0.3)'}}/>
 
         
         
