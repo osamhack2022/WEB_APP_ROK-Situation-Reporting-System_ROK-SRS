a5d4cc6c89b7d91fb3751635ff171ee362aa7df0
Brian Kim <s3kim2018@berkeley.edu>
2022-10-23 10:02:21 +0000

    [Feat]: #110 Chat scroll bug fixed, deisgn changes

--- front-end/web-next/pages/messages/[id].js
+++ front-end/web-next/pages/messages/[id].js
@@ -2,12 +2,11 @@ import Chatpage from '../messages'
 import Head from 'next/head'
 import { Avatar, Form, Select, Input } from 'antd';
 import styles from '../../styles/chatpage.module.css'
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { useRouter } from "next/router"
 import { collection, query, addDoc, updateDoc, doc } from "@firebase/firestore"
 import { getCookie } from 'cookies-next';
 import { decodeJwt } from 'jose';
-
 import { useCollection, useCollectionData } from "react-firebase-hooks/firestore"
 import { orderBy } from 'firebase/firestore';
 import { db } from '../../firebaseauth'
@@ -20,9 +19,15 @@ function getid() {
 }
 
 function chatpage() {
+    const [form] = Form.useForm();
+    const bottomOfChat = useRef();
     const router = useRouter()
     const { id } = router.query
-    
+
+    const scrollToBottom = useCallback(() => bottomOfChat.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    }), [])
 
     const [snapshot, loading, error] = useCollection(collection(db, "chats"))
     if (loading || !snapshot || !id) return <div>Loading...</div>
@@ -32,13 +37,29 @@ function chatpage() {
             chatdata = snapshot.docs[i].data()
         }
     }
+
     let submitnewmessage = async (values) => {
         console.log(values)
         let chattype = values.type
         let message = values.message
         if (!message) {
-            return 
+            return
         }
+        let endpoint = process.env.NEXT_PUBLIC_BACKEND_ROOT + 'api/chat/score?text='
+        const options = {
+            // The method is POST because we are sending data.
+            method: 'GET',
+            // Tell the server we're sending JSON.
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + getCookie('usercookie')
+            }
+        }
+        //Fetch data from external API
+        const res = await fetch(endpoint + message, options)
+        const data = await res.json()
+
+        let newscore = Math.round(0.4 * parseInt(chatdata['severity']) + 0.6 * parseInt(data.score))
         if (!chattype) {
             chattype = 'regular'
         }
@@ -52,13 +73,11 @@ function chatpage() {
         const collectionref = collection(db, "chats")
         await updateDoc(doc(collectionref, id), {
             recentmsg: message,
-            rectime: new Date()
+            rectime: new Date(),
+            severity: newscore
         })
-        values.message = ""
-
-
-
     }
+
 
     function Getparticipants() {
         let userdatas = chatdata['userdata']
@@ -72,54 +91,58 @@ function chatpage() {
             }
         }
     }
-    function Generatechat({ id }) {
+    function Generatechat({ id, scrollToBottom }) {
         const q = query(collection(db, "chats", id, "messages"), orderBy("timestamp"))
-        const [messages, loading, error] = useCollectionData(q)    
+        const [messages, loading, error] = useCollectionData(q)
+        useEffect(() => {
+            scrollToBottom()
+        }, [messages, scrollToBottom])
         return messages?.map(message => {
             let participant = Getparticipantbyid(message.sender)
             if (message.sender != getid()) {
                 return generatechatelement('theirs', message.type, message.text, participant.name, participant.color)
             } else {
                 return generatechatelement('mine', message.type, message.text, participant.name, participant.color)
-    
+
             }
         })
-    
+
     }
     return <>
         <Chatpage>
-            <div className = {styles.container}>
+            <div className={styles.container}>
 
-            <div className={styles.header}>
-                <div style={{ width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <h1 className={styles.title}>{chatdata.name}</h1>
-                    <div className={styles.participants}>
-                        <h3>참가자: </h3>
-                        <Avatar.Group>
-                            <Getparticipants></Getparticipants>
-                        </Avatar.Group>
+                <div className={styles.header}>
+                    <div style={{ width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <h1 className={styles.title}>{chatdata.name}</h1>
+                        <div className={styles.participants}>
+                            <h3>참가자: </h3>
+                            <Avatar.Group>
+                                <Getparticipants></Getparticipants>
+                            </Avatar.Group>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={styles.chatarea} style = {{flex: '1'}}>
-                <Generatechat id={id} />
-            </div>
-            <div className = {styles.footer}>
-                <Form initialValues={{remember: true}} className = {styles.messageform} onFinish={submitnewmessage}>
-                <Form.Item name="type">
-                    <Select id = "type" name = "type" defaultValue = "regular" placeholder="메세지 종류" optionFilterProp="children" style={{ width: '140px' }}>
-                        <Option value="regular">일반메세지</Option>
-                        <Option value="report">상황보고</Option>
-                        <Option value="order">지시사항</Option>
-                        <Option value="secret">암구호</Option>
-                    </Select>
-                </Form.Item>
-                <Form.Item name="message" style = {{flex: '1'}}>
-                    <Input id = "message" name = "message" placeholder="메세지를 입력해주세요"/>
-                </Form.Item>
-                <button className = {styles.button} type="primary">보내기</button>
-                </Form>
-            </div>
+                <div className={styles.chatarea} style={{ flex: '1' }}>
+                    <Generatechat id={id} scrollToBottom={scrollToBottom} />
+                    <div ref={bottomOfChat} />
+                </div>
+                <div className={styles.footer}>
+                    <Form form={form} initialValues={{ remember: true }} className={styles.messageform} onFinish={submitnewmessage}>
+                        <Form.Item name="type">
+                            <Select id="type" name="type" defaultValue="regular" placeholder="메세지 종류" optionFilterProp="children" style={{ width: '140px' }}>
+                                <Option value="regular">일반메세지</Option>
+                                <Option value="report">상황보고</Option>
+                                <Option value="order">지시사항</Option>
+                                <Option value="secret">암구호</Option>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item name="message" style={{ flex: '1' }}>
+                            <Input id="message" name="message" placeholder="메세지를 입력해주세요" />
+                        </Form.Item>
+                        <button className={styles.button} type="primary">보내기</button>
+                    </Form>
+                </div>
             </div>
 
 
@@ -171,7 +194,7 @@ function generatechatelement(which, type, content, name, color) {
                     <div className={styles.theirchatelem}>
                         {content}
                     </div>
-                    <div style = {{width: '50px', display: 'flex', flexDirection: 'column'}}>
+                    <div style={{ width: '50px', display: 'flex', flexDirection: 'column' }}>
                         <Avatar style={{ backgroundColor: color, margin: 'auto' }}> {name} </Avatar>
                         <div style={{ margin: 0, margin: 'auto' }}>{name}</div>
                     </div>
@@ -188,7 +211,7 @@ function generatechatelement(which, type, content, name, color) {
                         <p style={{ color: 'green', textAlign: 'center', fontWeight: 'bold' }}>[상황보고]</p>
                         {content}
                     </div>
-                    <div style = {{width: '50px', display: 'flex', flexDirection: 'column'}}>
+                    <div style={{ width: '50px', display: 'flex', flexDirection: 'column' }}>
                         <Avatar style={{ backgroundColor: color, margin: 'auto' }}> {name} </Avatar>
                         <div style={{ margin: 0, margin: 'auto' }}>{name}</div>
                     </div>
@@ -202,8 +225,8 @@ function generatechatelement(which, type, content, name, color) {
                         <p style={{ color: 'red', textAlign: 'center', fontWeight: 'bold' }}>[지시사항]</p>
                         {content}
                     </div>
-                    <div style = {{width: '50px', display: 'flex', flexDirection: 'column'}}>
-                        <Avatar style={{ backgroundColor: color, margin: 'auto'}}> {name} </Avatar>
+                    <div style={{ width: '50px', display: 'flex', flexDirection: 'column' }}>
+                        <Avatar style={{ backgroundColor: color, margin: 'auto' }}> {name} </Avatar>
                         <div style={{ margin: 0, margin: 'auto' }}>{name}</div>
                     </div>
 
@@ -219,7 +242,7 @@ function generatechatelement(which, type, content, name, color) {
                         {isVisible ? content : <>
                             클릭해 주세요.</>}
                     </div>
-                    <div style = {{width: '50px'}}>
+                    <div style={{ width: '50px' }}>
                         <Avatar style={{ backgroundColor: color, margin: 'auto' }}> {name} </Avatar>
                         <div style={{ margin: 0, margin: 'auto' }}>{name}</div>
                     </div>
