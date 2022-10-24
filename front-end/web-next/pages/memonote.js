f331f3fed047bcd31f17673c3faeb39053533c29
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-10-24 04:42:41 +0000

    [Feat]Parse korean time format
    mongoose의 timestamp를 한국 시간 형태로 parsing

--- front-end/web-next/pages/memonote.js
+++ front-end/web-next/pages/memonote.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Head from 'next/head'
 import { Layout, Row, Col, List, Button, Input, Divider, Select, Spin } from 'antd';
 import { FormOutlined } from '@ant-design/icons';
@@ -23,42 +23,17 @@ export default function Memo() {
     })
       .then(response => response.json())
       .then(data => setMemoRenderList(data));
-  }, [])
+  }, []);
 
-  const sampleData = [
-    {
-      'key': 0,
-      'name': 'Choe',
-      'position': '통신지원반',
-      'memo': 'XX시 XX분 부 위병소 앞 거수자 1명 식별. 긴급출동조는 지금 즉시 출동 바람.',
-      'datetime': '5분 전',
-      'title': '위병소 거수자 1명 식별',
-      'type': '긴급사항',
-      'level': 5,
-      'unit': '대대',
-      'isDone': false,
-      'comment': [
-        {
-          'name': 'Joe',
-          'position': '통신지원반',
-          'memo': '이상 무',
-          'datetime': '10분 전',
-        }
-      ]
-    },
-    {
-      'key': 1,
-      'name': 'Joe',
-      'position': '통신지원반',
-      'memo': '이상 무',
-      'datetime': '10분 전',
-      'title': '저녁 점호간 특이사항 보고',
-      'type': '보고사항',
-      'level': 0,
-      'unit': '중대',
-      'isDone': true
-    }
-  ];
+  const koreanTimeFormat = useCallback((UTCdate) => {
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const krDate = new Date(new Date(UTCdate) + KR_TIME_DIFF);
+    return (
+      '' + krDate.getFullYear() + '/' + (krDate.getMonth() + 1) + '/' + krDate.getDate()
+      + ' ' + (krDate.getHours() < 10 ? '0' + krDate.getHours() : krDate.getHours())
+      + ':' + (krDate.getMinutes() < 10 ? '0' + krDate.getMinutes() : krDate.getMinutes())
+    )
+  }, []);
 
   function Header(props) {
     return (
@@ -125,7 +100,7 @@ export default function Memo() {
                             popupClassName={Styles.siderTitle}
                             bordered={false}
                             value={memonoteType}
-                            onChange={(v) => { setMemonoteType(v); console.log(v) }}
+                            onChange={(value) => setMemonoteType(value)}
                           >
                             <Select.Option value="receiveMemo">받은 메모 보고</Select.Option>
                             <Select.Option value="sendMemo">보낸 메모 보고</Select.Option>
@@ -168,7 +143,7 @@ export default function Memo() {
                               justify="end"
                             >
                               <Col>중요도: {item.Severity}</Col>
-                              <Col>{item.createdAt}</Col>
+                              <Col>{koreanTimeFormat(item.createdAt)}</Col>
                             </Row>
                           </Button>
                           <Divider className={Styles.bottomDivider} />
@@ -188,7 +163,7 @@ export default function Memo() {
                           title={memoRenderList[selectedItem].Title}
                           type={memoRenderList[selectedItem].Type}
                           level={memoRenderList[selectedItem].Severity}
-                          datetime={memoRenderList[selectedItem].createdAt}
+                          datetime={koreanTimeFormat(memoRenderList[selectedItem].createdAt)}
                           status={memoRenderList[selectedItem].Status}
                         />
                       }
@@ -199,7 +174,7 @@ export default function Memo() {
                       name={memoRenderList[selectedItem].User?.Name}
                       position={memoRenderList[selectedItem].User?.Position}
                       memo={memoRenderList[selectedItem].Content}
-                      datetime={memoRenderList[selectedItem].createdAt}
+                      datetime={koreanTimeFormat(memoRenderList[selectedItem].createdAt)}
                       comment={memoRenderList[selectedItem].Comment}
                     />
                   </div>
