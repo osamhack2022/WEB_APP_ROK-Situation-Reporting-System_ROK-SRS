df3904f3e08e69b3c7ddcaa6629a297f51f6d909
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-10-30 13:51:58 +0000

    [Refactor]Remove unusing directories #121
    사용되지 않는 디렉토리 제거

--- front-end/app/src/helperfunctions/covertUserType.js
+++
@@ -1,9 +0,0 @@
-export function convertUserType(val) {
-  const userType = new Map()
-
-  userType['leader'] = '지휘자'
-  userType['commander'] = '지휘관'
-  userType['soldier'] = '병사'
-
-  return userType[val]
-}
--- front-end/app/src/states/index.js
+++
@@ -1,2 +0,0 @@
-export * from './unitState'
-export * from './userState'
